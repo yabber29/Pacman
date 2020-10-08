da0382ca445659e66d2675e0dd6c786d2a0d6909
yyashwan02 <72488082+yyashwan02@users.noreply.github.com>
2020-10-08 17:17:08 +0200

    Update pacman.js

--- pacman.js
+++ pacman.js
@@ -1,8 +1,8 @@
 function Pacman() {
   this.x = width / 2;
   this.y = height / 2;
-  this.d = scl * 0.75;
-  this.xdir = scl / 20;
+  this.d = scl * 0.8;
+  this.xdir = scl;// / 20;
   this.ydir = 0;
   this.mouthangle = 10;
   this.mouthchange = 3;
@@ -44,6 +44,7 @@ function Pacman() {
         this.x + this.d / 2 + this.mouthangle,
         this.y + this.d / 2 + 1);
     }
+
   }
   this.mouthmovement = function() {
     this.mouthangle += this.mouthchange;
@@ -53,32 +54,42 @@ function Pacman() {
   }
 
   this.move = function() {
+    var pacX, pacY;
+    var schild = 0.5;
     this.x += this.xdir;
     this.y += this.ydir;
     var collision = false;
-    if (this.xdir > 0) {
-      var pacX = this.x + this.d * 0.5;
+    if (this.ydir > 0) {
+      pacX = this.y + this.d * schild;
     } else {
-      var pacX = this.x - this.d * 0.5;
+      pacX = this.y - this.d * schild;
     }
-        if (this.xdir > 0) {
-      var pacY = this.x + this.d * 0.5;
+    if (this.xdir > 0) {
+      pacX = this.x + this.d * schild;
     } else {
-      var pacY = this.x - this.d * 0.5;
+      pacX = this.x - this.d * schild;
+    }
+    if (this.xdir > 0) {
+      pacY = this.x + this.d * schild;
+    } else {
+      pacY = this.x - this.d * schild;
     }
     if (this.ydir > 0){
-      var pacY = this.y + this.d * 0.5;
+      pacY = this.y + this.d * schild;
     } else {
-      var pacY = this.y - this.d * 0.5;
+      pacY = this.y - this.d * schild;
     }
     
     //console.log(this.x + "," + this.y);
     for (let i = 0; i < t.length; i++) {
       var wall = t[i];
       //console.log(wall.x + "," + wall.y + "/" + (wall.x + wall.w) + "," + (wall.y + wall.h));
-      if (((pacX > wall.x) && (pacX < wall.x + wall.w)) &&
-        ((pacY > wall.y) && (pacY < wall.y + wall.h)))
-        {
+      if (
+           (pacX >= wall.x - scl*0.5) &&
+           (pacX <= (wall.x + wall.w- scl*0.5)) &&
+           (pacY >= wall.y- scl*0.5) &&
+           (pacY <= (wall.y + wall.h- scl*0.5))
+        ) {
         //console.log("XXXXX + YYYYYY");
         collision = true;
       }
