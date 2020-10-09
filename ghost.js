bfe1853c300895410cab172decae7931749ac147
yyashwan02 <72488082+yyashwan02@users.noreply.github.com>
2020-10-09 09:27:58 +0200

    Create ghost.js

---
+++ ghost.js
@@ -0,0 +1,13 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(100);
+}
+
+function draw() {
+	fill(0, 0, 0);
+	fill(255, 0, 0);
+	circle(100, 100, 90);
+	fill(0, 0, 0)
+	circle(120,90,30);
+	circle(80,90,30);
+	line(100, 140, 100, 60);
